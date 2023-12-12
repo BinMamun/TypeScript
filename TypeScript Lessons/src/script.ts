18c5f486372bdb6077bff7e5f860887454fcf7f5
Abdullah Bin Mamun <binbean44@gmail.com>
2023-12-12 21:33:46 +0600

    Change folder name and merge

--- TypeScript Lessons/src/script.ts
+++ TypeScript Lessons/src/script.ts
@@ -1,3 +1,19 @@
+console.log("Hello World!");
+
+const add = ()=>{
+  const num1 = 25, num2 = 35;
+  console.log(num1 + num2);
+}
+add();
+
+function multiply(){
+  const num1 = 10, num2 = 15;
+  return num1 * num2;
+}
+
+console.log(multiply());
+
+
 let playerName:string = "Asif Mahmud";
 console.log(playerName);
 
