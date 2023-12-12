986a76cc492e81d65e38aad6b3c4918f5f3a2e91
Abdullah Bin Mamun <binbean44@gmail.com>
2023-12-12 18:36:59 +0600

    Lesson-3

---
+++ Lesson-3/script.js
@@ -0,0 +1,16 @@
+"use strict";
+let playerName = "Asif Mahmud";
+console.log(playerName);
+let age = 25;
+console.log(age);
+function multiply(a, b) {
+    return a * b;
+}
+console.log(multiply(25, 20));
+const fruits = ["Mango", "Apple", "Lichi"];
+console.log(fruits);
+const person = {
+    name: "Diellza",
+    age: 22
+};
+console.log(person);
---
+++ Lesson-3/script.ts
@@ -0,0 +1,24 @@
+let playerName:string = "Asif Mahmud";
+console.log(playerName);
+
+let age: number = 25;
+
+console.log(age);
+
+function multiply( a:number, b: number){
+  return a * b;
+}
+
+console.log(multiply(25, 20));
+
+
+const fruits:string[] = ["Mango", "Apple", "Lichi"];
+console.log(fruits);
+
+
+const person = {
+  name: "Diellza",
+  age: 22
+}
+
+console.log(person);
