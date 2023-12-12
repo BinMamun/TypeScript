b0e4e2c380377853d2325ecfc469fa7916aeaf56
Abdullah Bin Mamun <binbean44@gmail.com>
2023-12-12 18:13:44 +0600

    install tsc tsconfig jason file and change directories

---
+++ Lesson-2/js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+console.log("Hello World!");
+const add = () => {
+    const num1 = 25, num2 = 35;
+    console.log(num1 + num2);
+};
+add();
+function multiply() {
+    const num1 = 10, num2 = 15;
+    return num1 * num2;
+}
+console.log(multiply());
---
+++ Lesson-2/src/script.ts
@@ -0,0 +1,14 @@
+console.log("Hello World!");
+
+const add = ()=>{
+  const num1 = 25, num2 = 35;
+  console.log(num1 + num2);
+}
+add();
+
+function multiply(){
+  const num1 = 10, num2 = 15;
+  return num1 * num2;
+}
+
+console.log(multiply());
